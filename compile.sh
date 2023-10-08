028c018327af0278bd6a825bca5adb1b9a42b03a
Replit user <>
2023-10-08 02:41:02 +0000

    new structure

--- compile.sh
+++ compile.sh
@@ -2,14 +2,14 @@ rm -d -r -f out
 mkdir out
 cp t54.iso t54_OLD.iso
 rm t54.iso
-nasm -f elf boot.asm -o out/boot.o
+nasm -f elf src/boot.asm -o out/boot.o
 
 echo Comp C Source
-gcc -c  main.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/main.o
-gcc -c  vga.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/vga.o
-gcc -c  fs.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/fs.o
-gcc -c  cd.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/cd.o
-gcc -c  io.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/io.o
+gcc -c  src/main.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/main.o
+gcc -c  src/vga.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/vga.o
+gcc -c  src/fs.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/fs.o
+gcc -c  src/cd.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/cd.o
+gcc -c  src/io.c -m32 -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -W -o out/io.o
 
 ld -T link.ld -melf_i386 out/boot.o out/fs.o out/main.o out/vga.o out/cd.o out/io.o -o iso/boot/kernel
 

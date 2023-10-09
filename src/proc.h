af9109166474fd8c67cfd634711f29c28fe09181
timberlinecs <chinmaytanmay@outlook.com>
2023-10-09 12:20:32 -0600

    forgor

---
+++ src/proc.h
@@ -0,0 +1,15 @@
+/*
+    Maybe the hardest part of making this OS,
+    process managment (oh no)
+*/
+#ifndef PROC_H
+#define PROC_H
+
+// Process 
+typedef struct{
+    int PID; // Proc ID (in theory /BIN/INIT should be 0)
+} Process;
+
+
+
+#endif
